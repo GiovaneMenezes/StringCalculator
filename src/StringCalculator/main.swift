79dc3956d45e6cdae9bf85fa71977fed3eea79eb
Giovane Menezes <giovane.menezes@live.com>
2022-06-15 00:15:49 -0300

    enabling multiline on terminal

--- src/StringCalculator/main.swift
+++ src/StringCalculator/main.swift
@@ -8,8 +8,23 @@
 import Foundation
 
 while true {
+    print("Insert the number of lines:")
+    
+    guard let numberOfLinesString = readLine(),
+            let numberOfLines = Int(numberOfLinesString) else { continue }
+    
     print("Insert the string:")
-    if let str = readLine() {
-        print(try Calculator.add(numbers: str))
+    
+    var stringLines = [String]()
+    
+    for _ in 0..<numberOfLines {
+        if let str = readLine() {
+            stringLines.append(str)
+        }
     }
+    
+    print("The complete string is:\n\(stringLines.joined(separator: "\\n"))\n")
+    print("The result is:")
+    print(try Calculator.add(numbers: stringLines.joined(separator: "\n")))
+    print("\n\n\n\n")
 }
