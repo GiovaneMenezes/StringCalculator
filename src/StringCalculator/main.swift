365378ec7843ea148c21e32c05c058441f32a8d3
Giovane Menezes <giovane.menezes@live.com>
2022-06-15 00:25:16 -0300

    Improving string input on terminal

--- src/StringCalculator/main.swift
+++ src/StringCalculator/main.swift
@@ -8,23 +8,10 @@
 import Foundation
 
 while true {
-    print("Insert the number of lines:")
-    
-    guard let numberOfLinesString = readLine(),
-            let numberOfLines = Int(numberOfLinesString) else { continue }
-    
     print("Insert the string:")
-    
-    var stringLines = [String]()
-    
-    for _ in 0..<numberOfLines {
-        if let str = readLine() {
-            stringLines.append(str)
-        }
+    if let str = readLine() {
+        let preparedString = str.replacingOccurrences(of: "\\n", with: "\n")
+        print(try Calculator.add(numbers: preparedString))
     }
-    
-    print("The complete string is:\n\(stringLines.joined(separator: "\\n"))\n")
-    print("The result is:")
-    print(try Calculator.add(numbers: stringLines.joined(separator: "\n")))
-    print("\n\n\n\n")
+    print("\n")
 }
